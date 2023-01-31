f04fa4760839afa21be4f43cf38fde6aeff27b09
julio cesar <jcmcf2012@gmail.com>
2023-01-31 10:18:27 -0300

    feat: nestjs initial config

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { BlogModule } from './blog/blog.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
