55aa24273cc882a57d6700471f69d75d48250704
julio cesar <jcmcf2012@gmail.com>
2023-02-03 09:48:33 -0300

    feat: #1 add env for security

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,15 @@ import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
+    ConfigModule.forRoot(),
     JwtModule.register({
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '30s' },
     }),
   ],
