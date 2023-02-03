1ca6516c0cbba1b3b68e052030bb394100478105
julio cesar <jcmcf2012@gmail.com>
2023-02-02 21:02:27 -0300

    infra: adding routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { User } from 'src/schemas/user.schema';
+import { UsersService } from './users/users.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import {
+  Controller,
+  Get,
+  Request,
+  Post,
+  UseGuards,
+  Body,
+} from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService,
+    private readonly UsersService: UsersService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('users/profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('users/create')
+  async createUser(@Body() userDTO: User) {
+    console.log(userDTO);
+    return this.UsersService.create(userDTO);
   }
 }
